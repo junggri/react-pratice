0b173660bb88c6a8cd497b8444ef3880371e34b8
이정수 <junggri@ijeongsuui-MacBookPro.local>
2020-12-03 00:45:02 +0900

    201202

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import "./exercise";
+import {createStore} from "redux";
+import rootReducer from "./modules";
+import {Provider} from "react-redux";
 
+const store = createStore(rootReducer);
+console.log(store.getState());
 ReactDOM.render(
-   <App/>, document.getElementById('root')
-);
-
+   <Provider store={store}>
+      <App/>
+   </Provider>
+   , document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ learn-redux/src/modules/index.js
@@ -0,0 +1,7 @@
+import {combineReducers} from "redux";
+import counter from "./counter";
+import todos from "./toods";
+
+const rootReducer = combineReducers({counter, todos});
+
+export default rootReducer;
