a24a58ee9b9761045f3f3b7f38fd7ac6b27796d4
이정수 <junggri@ijeongsuui-MacBookPro.local>
2020-12-06 00:16:38 +0900

    201205

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Route, Link } from "react-router-dom";
 
-class App extends React.Component {
-   constructor(props) {
-      super(props);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.input = React.createRef();
-   }
+import Profiles from "./Profile";
 
-   handleSubmit(event) {
-      alert('A name was submitted: ' + this.input.current.value);
-      event.preventDefault();
-   }
-
-   render() {
-      return (
-         <form onSubmit={this.handleSubmit}>
-            <label>
-               Name:
-               <input
-                  type="text"
-               />
-            </label>
-            <input type="submit" value="Submit"/>
-         </form>
-      );
-   }
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+      </ul>
+      {/* <Route path="/" component={Home} exact={true} /> */}
+      {/* <Route path={["/about", "/info"]} component={About} /> */}
+      <Route path={"/profiles"} component={Profiles} />
+    </div>
+  );
 }
 
 export default App;
